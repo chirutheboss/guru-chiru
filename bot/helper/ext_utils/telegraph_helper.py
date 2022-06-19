2a3e886718bc8b0669429fe3c486d27bb96eec51
a7a8524 <64340223+darkmaster420@users.noreply.github.com>
2022-06-18 18:32:10 -0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,10 +72,10 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Mirror-leech-bot Torrent Search',
+                title = 'MLTB Torrent Search Results',
                 content=content
             )
         return
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('MLTB', 'https://github.com/darkmaster420/mirror-leech-telegram-bot')
